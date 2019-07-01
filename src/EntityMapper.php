291dac02403c1ff113ca6a4cf12584496e949370
Vitalik Shirokov <shirokov@intaro.ru>
2019-07-01 12:21:08 +0300

    fix #5

--- src/EntityMapper.php
+++ src/EntityMapper.php
@@ -35,14 +35,6 @@ class EntityMapper
         $class = get_class($object);
         $entityMap = EntityMap::fromClass($class);
 
-        $fields = array_filter($entityMap->getProperties(), function (PropertyMap $propertyMap) {
-            return $propertyMap->getAnnotation() instanceof Field;
-        });
-
-        $properties = array_filter($entityMap->getProperties(), function (PropertyMap $propertyMap) {
-            return $propertyMap->getAnnotation() instanceof Property;
-        });
-
         $data = self::entityToArray($entityMap, $object);
 
         // Сохраняем вложенные сущности
@@ -52,81 +44,179 @@ class EntityMapper
         $exist = self::getExistObjectRawResult($entityMap, $object);
 
         if ($exist && $exist->getId()) {
-            $changedData = array_udiff_assoc($data, $exist->getData(), function ($new, $old) {
-                $normalize = function ($value) {
-                    if ($value instanceof DateTime) {
-                        return $value->getTimestamp();
-                    }
-
-                    return $value;
-                };
-
-                $new = $new === null || $new === false || $new === [] ? false : array_map($normalize, (array)$new);
-                $old = $old === null || $old === false || $old === [] ? false : array_map($normalize, (array)$old);
-
-                return $new !== $old;
-            });
-
-            if (!$changedData) {
-                return $exist->getId();
-            }
-
-            $changedFields = array_filter($fields, function (PropertyMap $field) use ($changedData) {
-                return in_array($field->getReflection()->getName(), array_keys($changedData));
-            });
-
-            $bitrixFields = [];
-            if ($changedFields) {
-                foreach ($changedFields as $changedField) {
-                    $bitrixFields += self::getBitrixFieldEntry($changedField, $data);
-                }
-
-                $cIBlockElement = new CIBlockElement();
-                $isUpdated = $cIBlockElement->Update($exist->getId(), $bitrixFields);
-                self::assert($isUpdated, strip_tags($cIBlockElement->LAST_ERROR));
-            }
-
-            $changedProperties = array_filter($properties, function (PropertyMap $property) use ($changedData) {
-                return in_array($property->getReflection()->getName(), array_keys($changedData));
-            });
-
-            $bitrixProperties = [];
-            if ($changedProperties) {
-                foreach ($changedProperties as $changedProperty) {
-                    $bitrixProperties += self::getBitrixPropertyEntry($changedProperty, $data, $exist->getInfoBlockId());
-                }
-
-                CIBlockElement::SetPropertyValuesEx($exist->getId(), $exist->getInfoBlockId(), $bitrixProperties);
-            }
-
-            return $exist->getId();
+            return self::update($exist, $entityMap, $data);
         } else {
-            $infoBlockType = $entityMap->getAnnotation()->getType();
-            $infoBlockCode = $entityMap->getAnnotation()->getCode();
-            $infoBlock = self::getBitrixInfoBlock($infoBlockType, $infoBlockCode);
-            self::assert(!empty($infoBlock['ID']), "Не найден инфоблок с кодом $infoBlockCode и типом $infoBlockType.");
-
-            $bitrixFields = ['IBLOCK_ID' => $infoBlock['ID']];
-            foreach ($fields as $field) {
-                $bitrixFields += self::getBitrixFieldEntry($field, $data);
-            }
-
-            $bitrixProperties = [];
-            foreach ($properties as $property) {
-                $bitrixProperties += self::getBitrixPropertyEntry($property, $data, $infoBlock['ID']);
-            }
-
-            $addFields = $bitrixFields;
-            if ($bitrixProperties) {
-                $addFields['PROPERTY_VALUES'] = $bitrixProperties;
-            }
-
-            $cIBlockElement = new CIBlockElement();
-            $elementId = $cIBlockElement->Add($addFields);
-            self::assert($elementId, strip_tags($cIBlockElement->LAST_ERROR));
-
-            return $elementId;
+            return self::add($entityMap, $data);
         }
+    }
+
+    /**
+     * @param EntityMap $entityMap
+     * @param array $data
+     * @return int
+     * @throws Exception
+     */
+    protected static function add(EntityMap $entityMap, array $data)
+    {
+        $bitrixFields = self::getBitrixFields($entityMap, $data);
+        $bitrixProperties = self::getBitrixProperties($entityMap, $data, $bitrixFields['IBLOCK_ID']);
+
+        $addFields = $bitrixFields;
+        if ($bitrixProperties) {
+            $addFields['PROPERTY_VALUES'] = $bitrixProperties;
+        }
+
+        $cIBlockElement = new CIBlockElement();
+        $elementId = $cIBlockElement->Add($addFields);
+        self::assert($elementId, strip_tags($cIBlockElement->LAST_ERROR));
+
+        return $elementId;
+    }
+
+    /**
+     * @param EntityMap $entityMap
+     * @param array $data
+     * @return array
+     * @throws InvalidArgumentException
+     */
+    protected static function getBitrixFields(EntityMap $entityMap, array $data)
+    {
+        $fields = array_filter($entityMap->getProperties(), function (PropertyMap $propertyMap) {
+            return $propertyMap->getAnnotation() instanceof Field;
+        });
+
+        $infoBlockType = $entityMap->getAnnotation()->getType();
+        $infoBlockCode = $entityMap->getAnnotation()->getCode();
+        $infoBlock = self::getBitrixInfoBlock($infoBlockType, $infoBlockCode);
+        self::assert(!empty($infoBlock['ID']), "Не найден инфоблок с кодом $infoBlockCode и типом $infoBlockType.");
+
+        $bitrixFields = ['IBLOCK_ID' => $infoBlock['ID']];
+        foreach ($fields as $field) {
+            $bitrixFields += self::getBitrixFieldEntry($field, $data);
+        }
+
+        return $bitrixFields;
+    }
+
+    /**
+     * @param EntityMap $entityMap
+     * @param array $data
+     * @param int $infoBlockId
+     * @return array
+     * @throws Exception
+     */
+    protected static function getBitrixProperties(EntityMap $entityMap, array $data, $infoBlockId)
+    {
+        $properties = array_filter($entityMap->getProperties(), function (PropertyMap $propertyMap) {
+            return $propertyMap->getAnnotation() instanceof Property;
+        });
+
+        $bitrixProperties = [];
+        foreach ($properties as $property) {
+            $bitrixProperties += self::getBitrixPropertyEntry($property, $data, $infoBlockId);
+        }
+
+        return $bitrixProperties;
+    }
+
+    /**
+     * @param RawResult $exist
+     * @param EntityMap $entityMap
+     * @param array $data
+     * @return int
+     * @throws Exception
+     */
+    protected static function update(RawResult $exist, EntityMap $entityMap, array $data)
+    {
+        $changedData = self::getChangedData($exist->getData(), $data);
+
+        if (!$changedData) {
+            return $exist->getId();
+        }
+
+        self::updateBitrixFields($exist, $entityMap, $changedData);
+        self::updateBitrixProperties($exist, $entityMap, $changedData);
+
+        return $exist->getId();
+    }
+
+    /**
+     * @param array $exist
+     * @param array $data
+     * @return array
+     */
+    protected static function getChangedData(array $exist, array $data)
+    {
+        return array_udiff_assoc($data, $exist, function ($new, $old) {
+            $normalize = function ($value) {
+                if ($value instanceof DateTime) {
+                    return $value->getTimestamp();
+                }
+
+                return $value;
+            };
+
+            $new = $new === null || $new === false || $new === [] ? false : array_map($normalize, (array)$new);
+            $old = $old === null || $old === false || $old === [] ? false : array_map($normalize, (array)$old);
+
+            return $new !== $old;
+        });
+    }
+
+    /**
+     * @param RawResult $exist
+     * @param EntityMap $entityMap
+     * @param array $changedData
+     * @throws Exception
+     */
+    protected static function updateBitrixFields(RawResult $exist, EntityMap $entityMap, array $changedData)
+    {
+        $changedFields = array_filter($entityMap->getProperties(), function (PropertyMap $field) use ($changedData) {
+            return (
+                $field->getAnnotation() instanceof Field &&
+                in_array($field->getReflection()->getName(), array_keys($changedData))
+            );
+        });
+
+        if (!$changedFields) {
+            return;
+        }
+
+        $bitrixFields = [];
+        foreach ($changedFields as $changedField) {
+            $bitrixFields += self::getBitrixFieldEntry($changedField, $changedData);
+        }
+
+        $cIBlockElement = new CIBlockElement();
+        $isUpdated = $cIBlockElement->Update($exist->getId(), $bitrixFields);
+        self::assert($isUpdated, strip_tags($cIBlockElement->LAST_ERROR));
+    }
+
+    /**
+     * @param RawResult $exist
+     * @param EntityMap $entityMap
+     * @param array $changedData
+     * @throws Exception
+     */
+    protected static function updateBitrixProperties(RawResult $exist, EntityMap $entityMap, array $changedData)
+    {
+        $changedProperties = array_filter($entityMap->getProperties(), function (PropertyMap $property) use ($changedData) {
+            return (
+                $property->getAnnotation() instanceof Property &&
+                in_array($property->getReflection()->getName(), array_keys($changedData))
+            );
+        });
+
+        if (!$changedProperties) {
+            return;
+        }
+
+        $bitrixProperties = [];
+        foreach ($changedProperties as $changedProperty) {
+            $bitrixProperties += self::getBitrixPropertyEntry($changedProperty, $changedData, $exist->getInfoBlockId());
+        }
+
+        CIBlockElement::SetPropertyValuesEx($exist->getId(), $exist->getInfoBlockId(), $bitrixProperties);
     }
 
     /**
