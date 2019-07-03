ae7b61766318fce6d501f1d4c4ddcc2d6ee4baf8
Vitalik Shirokov <shirokov@intaro.ru>
2019-07-03 12:11:00 +0300

    change phpunit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,35 +1,14 @@
 <?php
 
-use Sheerockoff\BitrixCi\SqlDump;
+use Sheerockoff\BitrixCi\Bootstrap;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$db = mysqli_connect(
-    getenv('MYSQL_HOST'),
-    getenv('MYSQL_USER'),
-    getenv('MYSQL_PASSWORD'),
-    getenv('MYSQL_DATABASE')
-);
+echo "SQL dump migrating...";
+Bootstrap::migrate();
+echo "COMPLETE\n";
 
-if (!$db) {
-    exit('Mysql connection error.');
-}
-
-$sqlDump = new SqlDump(__DIR__ . '/../vendor/sheerockoff/bitrix-ci/dump.sql');
-foreach ($sqlDump->parse() as $query) {
-    mysqli_query($db, $query);
-}
-
-mysqli_close($db);
-
-define('NOT_CHECK_PERMISSIONS', true);
-define('NO_AGENT_CHECK', true);
-
-$_SERVER['DOCUMENT_ROOT'] = realpath(__DIR__ . '/../vendor/sheerockoff/bitrix-ci/files/');
-/** @noinspection PhpIncludeInspection */
-require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php');
-
-CModule::IncludeModule('iblock');
+Bootstrap::bootstrap();
 
 require __DIR__ . '/resources/Entity/Book.php';
 require __DIR__ . '/resources/Entity/Author.php';
